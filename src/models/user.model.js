9cc15d1d53ae1914f95e0179bba31716561849ba
aditya <vinchuaditya@gmail.com>
2022-04-26 13:01:46 +0530

    login validation added

---
+++ src/models/user.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    fname: {
+      type: String
+    },
+    lname: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    password: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('User', userSchema);
