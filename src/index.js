7116c6c087130edf6ba8339fcba90d7acdc22581
aditya <vinchuaditya@gmail.com>
2022-05-10 15:29:33 +0530

    [Aditya]added: API documentation using swagger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../src/swagger/swagger.json'
+
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -26,6 +29,8 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 database();
 
